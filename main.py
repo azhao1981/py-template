3929738d3c6c004840b8435bf6239873d0ac0f5d
weiz <azhao.1981@gmail.com>
2025-01-06 20:20:50 +0800

    doc

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import requests
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(),override=True)
 
+
 def main():
-  response = requests.get('https://ifconfig.me')
-  print(response.text)
+    response = requests.get('https://ifconfig.me')
+    print(response.text)
+
 
 if __name__ == "__main__":
-  main()
+    main()
